9726dcc375cff39e349ec861c6b50a6606b1b994
Fernando <fernando_felicio82@yahoo.com.br>
2022-02-24 01:38:41 -0300

    Módulo 1 - Entendendo o uso dos widgets
    - explicação da função runApp;
    - uso de aninhamento de widgets com o uso do Column.

--- modulo-1-fundamentos/bytebank/lib/main.dart
+++ modulo-1-fundamentos/bytebank/lib/main.dart
@@ -1,68 +1,43 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // A função abaixo é o ponto de partida do app
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+  // A função runApp é o ponto de partida do app
+  // Na função runApp só pode incluir um widget
+  // O que estiver dentro dessa função será a base do app
+  runApp(
+    // o widget Column nos permite aninhar vários componentes de widgets.
+    // Ele recebe uma lista de widgets e seu direcionamento se dá pela horizontal
+    Column(
+      children: <Widget>[
+        const Text(
+          'Deliver features faster',
+          // como o widget text não possui uma direção específica, foi incluído o atributo textDirection.
+          // sem ele é apresentado um erro no app
+          textDirection: TextDirection.ltr,
+        ),
+        const Text(
+          'Craft beautiful UIs',
+          textDirection: TextDirection.ltr,
+        ),
+        const Text(
+          'Craft beautiful UIs',
+          textDirection: TextDirection.ltr,
+        ),
+        Column(
+          children: const <Widget>[
+            Text(
+              'Craft beautiful UIs',
+              textDirection: TextDirection.ltr,
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              'Deliver features faster',
+              // como o widget text não possui uma direção específica, foi incluído o atributo textDirection.
+              // sem ele é apresentado um erro no app
+              textDirection: TextDirection.ltr,
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
+      ], //<Widget>[]
+    ),
+  );
 }
