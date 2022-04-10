e7fd631d7a2f237effff179367d521d198a0ff03
Fernando <fernando_felicio82@yahoo.com.br>
2022-04-10 07:17:22 -0300

    Aula 2 - Implementando layout com Widgets
    - Apresentação do widget MaterialApp (responsável pela estrutura do app);
    - Apresentação do widget Scaffold. Utilizado como esqueleto do app;
    - Apresentação da appBar e FloatingActionButton.

--- modulo-1-fundamentos/bytebank/lib/main.dart
+++ modulo-1-fundamentos/bytebank/lib/main.dart
@@ -1,43 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // A função runApp é o ponto de partida do app
-  // Na função runApp só pode incluir um widget
-  // O que estiver dentro dessa função será a base do app
   runApp(
-    // o widget Column nos permite aninhar vários componentes de widgets.
-    // Ele recebe uma lista de widgets e seu direcionamento se dá pela horizontal
-    Column(
-      children: <Widget>[
-        const Text(
-          'Deliver features faster',
-          // como o widget text não possui uma direção específica, foi incluído o atributo textDirection.
-          // sem ele é apresentado um erro no app
-          textDirection: TextDirection.ltr,
+    /*
+     * Esse widget envolverá todos os outros componentes que adicionarmos, e é
+     * responsável pelos comportamentos esperados por padrão nessa implementação,
+     * seja na parte de navegação, transição e assim por diante.
+     */
+    MaterialApp(
+      // home: indica o widget que irá representar a primeira tela
+      // Scaffold: permite implementar toda a estrutura básica (ou esqueleto) do Material Design
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Transferências'),
         ),
-        const Text(
-          'Craft beautiful UIs',
-          textDirection: TextDirection.ltr,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {}, // obrigado a passar
+          // permite definir o conteúdo do FloatingActionButton
+          child: const Icon(Icons.add),
         ),
-        const Text(
-          'Craft beautiful UIs',
-          textDirection: TextDirection.ltr,
-        ),
-        Column(
-          children: const <Widget>[
-            Text(
-              'Craft beautiful UIs',
-              textDirection: TextDirection.ltr,
-            ),
-            Text(
-              'Deliver features faster',
-              // como o widget text não possui uma direção específica, foi incluído o atributo textDirection.
-              // sem ele é apresentado um erro no app
-              textDirection: TextDirection.ltr,
-            ),
-          ],
-        ),
-      ], //<Widget>[]
+      ),
     ),
   );
 }
