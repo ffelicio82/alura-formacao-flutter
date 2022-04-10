90029836efb538c4a00d210d8e29e41305071983
Fernando <fernando_felicio82@yahoo.com.br>
2022-04-10 08:49:04 -0300

    Módulo 01 - Aula 2 - refatorando o código
    **Módulo 01 - Aula 02 - Implementando layout com Widgets - atividade 06 - refatorando o código**
    
    - Apresentação do recurso de herança(para criar componentes reutilizáveis);
    - Explicação da herança com **Widget**(criação de componentes de baixo nível) e Stateless/StatefulWidget(para aproveitamento de componentes já criados);
    - Separação dos componentes de lista de cards e itens para melhor reaproveitamento;
    - Criação da classe Transferencia para estruturar as informações.

--- modulo-1-fundamentos/bytebank/lib/main.dart
+++ modulo-1-fundamentos/bytebank/lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      // remove o banner de debug do app
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
@@ -13,17 +12,68 @@ void main() {
           onPressed: () {},
           child: const Icon(Icons.add),
         ),
-        // O widget Card é um container para outros conteúdos
-        body: const Card(
-          // ListTile: componente responsável por criar estruturas de listas.
-          child: ListTile(
-            // leading: conteúdo principal (pode ser qualquer widget)
-            leading: Icon(Icons.monetization_on),
-            title: Text('100.00'),
-            subtitle: Text('1000'),
-          ),
-        ),
+        body: const ListaTransferencias(),
       ),
     ),
   );
+}
+
+/*
+  * Existem 2 formas de criação de widgets
+  * Classe extends Widget
+  * - mais utilizada para construção de widgets de baixo nível.
+  * Classe extends StatelessWidget/StatefulWidget
+  * - utilizado para usar um código que já foi escrito.
+  * - Essas classes também herdam de Widget, mas têm o comportamento de encapsular
+  * códigos de Widgets já prontos.
+  * A diferença entre elas é que StatefulWidget tem a capacidade de modificar o conteúdo
+  * do widget de maneira dinâmica a partir de alguns eventos que acontecem no aplicativo,
+  * enquanto o StatefulWidget não permite esse tipo de modificação.
+ */
+class ListaTransferencias extends StatelessWidget {
+  const ListaTransferencias({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [
+        ItemTransferencia(
+          transferencia: Transferencia(valor: 100, numeroConta: 1000),
+        ),
+        ItemTransferencia(
+          transferencia: Transferencia(valor: 200, numeroConta: 2000),
+        ),
+        ItemTransferencia(
+          transferencia: Transferencia(valor: 300, numeroConta: 3000),
+        ),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia transferencia;
+
+  const ItemTransferencia({
+    Key? key,
+    required this.transferencia,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: const Icon(Icons.monetization_on),
+        title: Text(transferencia.valor.toString()),
+        subtitle: Text(transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  const Transferencia({required this.valor, required this.numeroConta});
 }
